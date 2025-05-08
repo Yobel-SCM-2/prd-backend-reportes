59f2d908026a95616d8fd4ff17a8a8083971dbdf
TadeoPG <tadeomanuelportillo@gmail.com>
2025-05-08 16:42:37 -0500

    :fire: first commit

---
+++ src/main/java/com/optimus/prdbackendreportes/PrdBackendReportesApplication.java
@@ -0,0 +1,13 @@
+package com.optimus.prdbackendreportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrdBackendReportesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrdBackendReportesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/optimus/prdbackendreportes/PrdBackendReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimus.prdbackendreportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrdBackendReportesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
