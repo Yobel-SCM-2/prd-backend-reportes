373b289174e7df20c05986babce49e18b96a1809
TadeoPG <tadeomanuelportillo@gmail.com>
2025-06-30 11:07:29 -0500

    refactor: change folder and names for repositories

---
+++ src/main/java/com/optimus/prdbackendreportes/domain/repository/DeliveryReceiptRepository.java
@@ -0,0 +1,28 @@
+package com.optimus.prdbackendreportes.domain.repository;
+
+import com.optimus.prdbackendreportes.domain.model.entity.DeliveryReceiptItem;
+import com.optimus.prdbackendreportes.domain.model.valueobject.Account;
+import com.optimus.prdbackendreportes.domain.model.valueobject.OrderNumber;
+import com.optimus.prdbackendreportes.domain.model.valueobject.ProcessBatch;
+import com.optimus.prdbackendreportes.domain.model.valueobject.ProcessDate;
+
+import java.util.List;
+
+public interface DeliveryReceiptRepository {
+
+    List<DeliveryReceiptItem> findDeliveryReceiptData(
+            Account account,
+            ProcessDate processDate,
+            ProcessBatch processBatch,
+            OrderNumber orderNumber,
+            String schema
+    );
+
+    boolean existsDataFor(
+            Account account,
+            ProcessDate processDate,
+            ProcessBatch processBatch,
+            OrderNumber orderNumber,
+            String schema
+    );
+}
---
+++ src/main/java/com/optimus/prdbackendreportes/domain/repository/ProcessedPickupsRepository.java
@@ -0,0 +1,24 @@
+package com.optimus.prdbackendreportes.domain.repository;
+
+import com.optimus.prdbackendreportes.domain.model.entity.DetailInfo;
+import com.optimus.prdbackendreportes.domain.model.entity.HeaderInfo;
+import com.optimus.prdbackendreportes.domain.model.valueobject.Account;
+import com.optimus.prdbackendreportes.domain.model.valueobject.ProcessBatch;
+import com.optimus.prdbackendreportes.domain.model.valueobject.ProcessDate;
+
+import java.util.List;
+
+public interface ProcessedPickupsRepository {
+
+    List<HeaderInfo> findHeaderInfo(
+            Account account,
+            ProcessDate processDate,
+            ProcessBatch processBatch
+    );
+
+    List<DetailInfo> findDetailInfo(
+            Account account,
+            ProcessDate processDate,
+            ProcessBatch processBatch
+    );
+}
