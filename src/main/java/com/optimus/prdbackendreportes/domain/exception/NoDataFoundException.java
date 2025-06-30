474874132ba2d7c79f495f9fc4bd88d57af81692
TadeoPG <tadeomanuelportillo@gmail.com>
2025-06-30 11:26:12 -0500

    refactor: change exceptions folder

---
+++ src/main/java/com/optimus/prdbackendreportes/domain/exception/NoDataFoundException.java
@@ -0,0 +1,7 @@
+package com.optimus.prdbackendreportes.domain.exception;
+
+public class NoDataFoundException extends RuntimeException {
+    public NoDataFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/optimus/prdbackendreportes/domain/exception/ReportGenerationException.java
@@ -0,0 +1,7 @@
+package com.optimus.prdbackendreportes.domain.exception;
+
+public class ReportGenerationException extends RuntimeException {
+    public ReportGenerationException(String message) {
+        super(message);
+    }
+}
