fa099f1e481cb829833fa5acfce27edd4d8233f4
TadeoPG <tadeomanuelportillo@gmail.com>
2025-06-30 17:16:01 -0500

    feat: add other constructor to ReportGenerationException

--- src/main/java/com/optimus/prdbackendreportes/domain/exception/ReportGenerationException.java
+++ src/main/java/com/optimus/prdbackendreportes/domain/exception/ReportGenerationException.java
@@ -4,4 +4,8 @@ public class ReportGenerationException extends RuntimeException {
     public ReportGenerationException(String message) {
         super(message);
     }
+
+    public ReportGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
